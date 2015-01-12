700798b7b5e95d31f532aa770318b25b30b74705
Chris Rodriguez <crodriguez@edx.org>
2015-01-12 15:57:20 -0500

    Video player branding addition for XuetangX

--- cms/envs/test.py
+++ cms/envs/test.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 This config file runs the simplest dev environment using sqlite, and db-based
 sessions. Assumes structure:
@@ -231,5 +232,16 @@ FEATURES['ENABLE_DISCUSSION_SERVICE'] = False
 
 # Enable content libraries code for the tests
 FEATURES['ENABLE_CONTENT_LIBRARIES'] = True
-
 FEATURES['ENABLE_EDXNOTES'] = True
+VIDEO_CDN_URL = {
+    'CN': 'http://api.xuetangx.com/edx/video?s3_url='
+}
+VIDEO_CDN_INFO = {
+    'CN': {
+        'BRANDING': {
+            'url': 'http://www.xuetangx.com',
+            'logo_src': 'http://www.xuetangx.com/static/images/logo.png',
+            'logo_tag': _('Video hosted by XuetangX.com'),
+        },
+    },
+}
